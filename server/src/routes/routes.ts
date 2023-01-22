0aebc54715db518462fbd3c7c751822c5b07d65a
Dants0 <gui.dg1@hotmail.com>
2023-01-22 16:51:13 -0300

    backend finalizado?

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,19 +1,19 @@
 import { prisma } from "../controller/prisma"
 import { FastifyInstance } from "fastify"
-import { z } from 'zod' 
+import { z } from 'zod'
 import dayjs from 'dayjs'
 
-export async function appRoutes(app: FastifyInstance){
-    app.post('/habits', async(request)=>{
+export async function appRoutes(app: FastifyInstance) {
+    app.post('/habits', async (request) => {
         const createHabitBody = z.object({
             title: z.string(),
             weekDays: z.array(
                 z.number().min(0).max(6)
-                )
+            )
         })
 
         const { title, weekDays } = createHabitBody.parse(request.body)
-        
+
         const today = dayjs().startOf('day').toDate()
 
         await prisma.habit.create({
@@ -28,10 +28,10 @@ export async function appRoutes(app: FastifyInstance){
                     })
                 }
             }
-        })  
+        })
     })
 
-    app.get('/day', async(request)=>{
+    app.get('/day', async (request) => {
         const getDayParams = z.object({
             date: z.coerce.date() //converte o parametro para data
         })
@@ -43,11 +43,11 @@ export async function appRoutes(app: FastifyInstance){
         const weekDay = parsedDate.get('day')
 
         const possibleHabits = await prisma.habit.findMany({
-            where:{
+            where: {
                 created_at: {
                     lte: date,
                 },
-                weekDays:{
+                weekDays: {
                     some: {
                         week_day: weekDay,
                     }
@@ -56,7 +56,7 @@ export async function appRoutes(app: FastifyInstance){
         })
 
         const day = await prisma.day.findUnique({
-            where:{
+            where: {
                 date: parsedDate.toDate(),
             },
             include: {
@@ -74,5 +74,81 @@ export async function appRoutes(app: FastifyInstance){
             possibleHabits,
             completedHabits
         }
+    })
+
+    //completar ou não completar o hábito
+    app.patch('/habits/:id/toggle', async (request) => {
+        const toggleHabitParams = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = toggleHabitParams.parse(request.params)
+
+        const today = dayjs().startOf('day').toDate()
+
+        let day = await prisma.day.findUnique({
+            where: {
+                date: today
+            },
+        })
+
+        if (!day) {
+            day = await prisma.day.create({
+                data: {
+                    date: today
+                }
+            })
+        }
+
+        const dayHabit = await prisma.dayHabit.findUnique({
+            where: {
+                day_id_habit_id: {
+                    day_id: day.id,
+                    habit_id: id
+                }
+            }
+        })
+
+        if (dayHabit) {
+            await prisma.dayHabit.delete({
+                where: {
+                    id: dayHabit.id
+                }
+            })
+        } else {
+            await prisma.dayHabit.create({
+                data: {
+                    day_id: day.id,
+                    habit_id: id
+                }
+            })
+        }
+
+    })
+
+    app.get('/summary', async () => {
+        const summary = await prisma.$queryRaw`
+            SELECT 
+            D.id, 
+            D.date, 
+            (
+                select 
+                cast(count(*) as float)
+                from day_habits DH
+                where DH.day_id = D.id
+            ) as completed,
+            (
+                select
+                cast(count(*) as float)
+                from habit_week_days HWD
+                join habits H
+                    on H.id = HWD.habit_id
+                where 
+                HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+                and H.created_at <= D.date
+            ) as amount
+            from days D
+        `
+        return summary
     })
 }
