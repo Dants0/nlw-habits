91efefe3a508a5d61666c427b452c1e61745c6f9
Dants0 <gui.dg1@hotmail.com>
2023-01-22 21:22:39 -0300

    finish 03, web

--- web/src/components/Summary.tsx
+++ web/src/components/Summary.tsx
@@ -26,7 +26,13 @@ export function Summary() {
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map(date => {
-                    return <HabitsDays key={date.toString()}/>
+                    return (
+                    <HabitsDays
+                    key={date.toString()} 
+                    amount={5} 
+                    completed={Math.round(Math.random() * 2)} 
+                    />
+                    )
                 })}
 
                 {amountOfDays > 0 && Array.from({length: amountOfDays}).map((_, i) => {
